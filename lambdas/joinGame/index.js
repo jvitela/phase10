70d3cd6f54b4a4cd14c08eaef9e00fefcfdf0532
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-04-28 00:33:39 +0200

    removed commented code

--- lambdas/joinGame/index.js
+++ lambdas/joinGame/index.js
@@ -1,9 +1,4 @@
 const AWS = require("aws-sdk");
-// const Player = require("/opt/phase10/entities/Player");
-// const ValidationError = require("/opt/phase10/entities/ValidationError");
-// const ResponseAction = require("/opt/phase10/entities/ResponseAction");
-// const GameRepository = require("/opt/phase10/repositories/GameRepository");
-// const PlayersRepository = require("/opt/phase10/repositories/PlayersRepository");
 const { joinGame } = require("./handler");
 
 AWS.config.update({ region: process.env.AWS_REGION });
