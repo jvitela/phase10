82bebd47c3b7a4f412945501c760af4a39f0c5fa
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-04-27 23:20:29 +0200

    changed implementation for TestUtils

--- lambdaLayers/TestUtils.js
+++ lambdaLayers/TestUtils.js
@@ -1,21 +1,20 @@
 const fnSuccessReq = (resp) =>
   jest.fn(() => ({
-    promise: async () => resp,
+    promise: () =>
+      new Promise((resolve) => setTimeout(() => resolve(resp), 100)),
   }));
 
 const fnErrorReq = (err) =>
   jest.fn(() => ({
-    promise: async () => {
-      throw err;
-    },
+    promise: () =>
+      new Promise((_, reject) => setTimeout(() => reject(err), 100)),
   }));
 
-const fnSuccess = (resp) => jest.fn(async () => resp);
+const fnSuccess = (resp) =>
+  jest.fn(() => new Promise((resolve) => setTimeout(() => resolve(resp), 100)));
 
 const fnError = (err) =>
-  jest.fn(async () => {
-    throw err;
-  });
+  jest.fn(() => new Promise((_, reject) => setTimeout(() => reject(err), 100)));
 
 module.exports = {
   fnSuccessReq,
