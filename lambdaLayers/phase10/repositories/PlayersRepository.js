26741e31b6da92f61385013db06ab81529141508
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-04-28 17:56:08 +0200

    allow new players to take abandoned spots

--- lambdaLayers/phase10/repositories/PlayersRepository.js
+++ lambdaLayers/phase10/repositories/PlayersRepository.js
@@ -1,5 +1,5 @@
 const ValidationError = require("../entities/ValidationError");
-const isValidName = /^[a-zA-Z0-9.!_ ]{3,}$/;
+const isValidName = /^[a-zA-Z0-9.!_ ]{3,15}$/;
 const MAX_PLAYERS = 6;
 
 class PlayersRepository {
@@ -13,11 +13,8 @@ class PlayersRepository {
     }
 
     const state = this.game.state;
-    if (state.activePlayer !== 0) {
-      // Abort if a game is already started
-      throw new ValidationError("game_already_started");
-    }
 
+    // Check if player is already in the list
     const hasPlayer = state.players.find(
       (player) => player.connectionId === newPlayer.connectionId
     );
@@ -25,18 +22,27 @@ class PlayersRepository {
       return;
     }
 
-    if (state.players.length === MAX_PLAYERS) {
-      const freeIdx = state.players.findIndex(
-        (player) => player.connectionId === null
-      );
-      if (freeIdx === -1) {
-        throw new ValidationError("game_players_full");
-      }
+    // Check if there is a free spot
+    const freeIdx = state.players.findIndex(
+      (player) => player.connectionId === null
+    );
+
+    if (freeIdx === -1 && state.activePlayer !== null) {
+      // Abort if a game is already started
+      //  and there are no abandoned players
+      throw new ValidationError("game_already_started");
+    }
+
+    if (freeIdx !== -1) {
+      // Take free spot
       newPlayer.color = freeIdx;
       state.players[freeIdx] = newPlayer;
-    } else {
+    } else if (state.players.length < MAX_PLAYERS) {
+      // Add new player
       newPlayer.color = state.players.length;
       state.players.push(newPlayer);
+    } else {
+      throw new ValidationError("game_players_full");
     }
   }
 }
