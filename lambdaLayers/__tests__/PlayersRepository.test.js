e25664e73f4781315d35f79e2f16dd561b405cc9
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-04-28 17:54:44 +0200

    Fixed tests with promises

--- lambdaLayers/__tests__/PlayersRepository.test.js
+++ lambdaLayers/__tests__/PlayersRepository.test.js
@@ -22,42 +22,70 @@ describe("PlayersRepository", () => {
 });
 
 describe("PlayersRepository::add", () => {
-  test("fails if the name is invalid", () => {
+  test("fails if the name is invalid", async () => {
     const game = { state: {} };
     const players = new PlayersRepository(game);
-    let rejects = expect(players.add(new Player())).rejects;
-    rejects.toBeInstanceOf(ValidationError);
-    rejects.toThrow("invalid_name");
 
-    rejects = expect(players.add(new Player("1a", "$$$"))).rejects;
-    rejects.toBeInstanceOf(ValidationError);
-    rejects.toThrow("invalid_name");
+    // Name is missing
+    let rejects = expect(players.add(new Player())).rejects;
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("invalid_name"),
+    ]);
+
+    // Name has invalid characters
+    rejects = expect(players.add(new Player("1a", "a$b%"))).rejects;
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("invalid_name"),
+    ]);
+
+    // Name is too short (min 3)
+    rejects = expect(players.add(new Player("1a", "Aa"))).rejects;
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("invalid_name"),
+    ]);
+
+    // Name is too long (max 15)
+    rejects = expect(players.add(new Player("1a", "abcdefghijklmnop"))).rejects;
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("invalid_name"),
+    ]);
   });
 
-  test("fails if a game has already started", () => {
+  test("fails if a game has already started and there are not free spots", async () => {
     const game = {
-      state: { activePlayer: 2 },
+      state: {
+        activePlayer: "b",
+        players: [new Player("a", "One", 0), new Player("b", "Two", 1)],
+      },
     };
     const players = new PlayersRepository(game);
-    const player = new Player("1", "John Doe");
+    const player = new Player(3, "John Doe");
     const rejects = expect(players.add(player)).rejects;
-    rejects.toBeInstanceOf(ValidationError);
-    rejects.toThrow("game_already_started");
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("game_already_started"),
+    ]);
   });
 
-  test("fails if the game is full", () => {
+  test("fails if the game is full", async () => {
     const players = [];
-    for (let i = 1; i <= PlayersRepository.MAX_PLAYERS; ++i) {
-      players.push(new Player(`p-${i}`, `Player ${i}`));
+    for (let i = 0; i < PlayersRepository.MAX_PLAYERS; ++i) {
+      players.push(new Player(`p-${i}`, `Player ${i + 1}`, i));
     }
     const game = {
-      state: { players, activePlayer: 0 },
+      state: { players, activePlayer: null },
     };
     const repo = new PlayersRepository(game);
     const player = new Player("x", "John Doe");
     const rejects = expect(repo.add(player)).rejects;
-    rejects.toBeInstanceOf(ValidationError);
-    rejects.toThrow("game_players_full");
+    await Promise.all([
+      rejects.toBeInstanceOf(ValidationError),
+      rejects.toThrow("game_players_full"),
+    ]);
   });
 
   test("ignore when player joined aready", async () => {
@@ -65,7 +93,7 @@ describe("PlayersRepository::add", () => {
     const game = {
       state: {
         players: [{ ...john }],
-        activePlayer: 0,
+        activePlayer: null,
       },
     };
 
@@ -79,7 +107,7 @@ describe("PlayersRepository::add", () => {
     const game = {
       state: {
         players: [],
-        activePlayer: 0,
+        activePlayer: null,
       },
     };
     const john = new Player("1", "John Doe");
@@ -98,7 +126,7 @@ describe("PlayersRepository::add", () => {
     const game = {
       state: {
         players: [john, jane],
-        activePlayer: 0,
+        activePlayer: null,
       },
     };
 
@@ -110,5 +138,30 @@ describe("PlayersRepository::add", () => {
     expect(game.state.players[1]).toBe(jane);
     expect(game.state.players[2]).toBe(max);
     expect(game.state.players[2].color).toBe(2);
+  });
+
+  test("reuse spot of abandoned player", async () => {
+    const john = new Player("a", "John", 0);
+    const juan = new Player(null, "Juan", 1);
+    const jane = new Player("c", "Jane", 2);
+
+    // Juan is disconnected
+    const game = {
+      state: {
+        players: [john, juan, jane],
+        activePlayer: "a",
+      },
+    };
+
+    // We add a new player named Max
+    const players = new PlayersRepository(game);
+    const max = new Player("d", "Max");
+    await players.add(max);
+
+    expect(game.state.players.length).toBe(3);
+    expect(game.state.players[0]).toBe(john);
+    expect(game.state.players[1]).toBe(max);
+    expect(game.state.players[2]).toBe(jane);
+    expect(max.color).toBe(1);
   });
 });
