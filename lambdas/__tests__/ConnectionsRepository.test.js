70882ff209da5e535d11f7530506f0c0d0377099
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-05-10 13:07:38 +0200

    Added new class to send messages using apigwManagementApi

---
+++ lambdas/__tests__/ConnectionsRepository.test.js
@@ -0,0 +1,79 @@
+const { fnSuccessReq, fnErrorReq } = require("/opt/TestUtils");
+const ConnectionsRepository = require("/opt/phase10/repositories/ConnectionsRepository");
+
+describe("ConnectionsRepository", () => {
+  test("export", () => {
+    expect(ConnectionsRepository).toBeInstanceOf(Function);
+  });
+
+  test("constructor", () => {
+    const comms = new ConnectionsRepository();
+    expect(comms).toBeInstanceOf(ConnectionsRepository);
+  });
+
+  test("properties", () => {
+    const apigwManagementApi = {};
+    const comms = new ConnectionsRepository(apigwManagementApi);
+    expect(comms.apigwManagementApi).toBe(apigwManagementApi);
+  });
+});
+
+describe("ConnectionsRepository::postToAll", () => {
+  test("Ignores connections without id", async () => {
+    const apigwManagementApi = {
+      postToConnection: fnErrorReq(),
+    };
+    const connections = [{ id: null }, { id: null }];
+    const comms = new ConnectionsRepository(apigwManagementApi);
+    const errors = await comms.postToAll(connections, null);
+    expect(apigwManagementApi.postToConnection.mock.calls.length).toBe(0);
+    expect(errors).toBe(false);
+  });
+
+  test("Resets connections that failed with 410", async () => {
+    const apigwManagementApi = {
+      postToConnection: fnErrorReq({ statusCode: 410, message: "Gone" }),
+    };
+    const connections = [{ id: "1a" }];
+    const comms = new ConnectionsRepository(apigwManagementApi);
+    const errors = await comms.postToAll(connections, null);
+    expect(apigwManagementApi.postToConnection.mock.calls.length).toBe(1);
+    expect(errors).toBe(true);
+    expect(connections[0].id).toBeNull();
+  });
+
+  test("JSON encodes data sent to connections", async () => {
+    const apigwManagementApi = {
+      postToConnection: fnSuccessReq(),
+    };
+    const msg = { foo: "bar" };
+    const connections = [{ id: "1a" }];
+    const comms = new ConnectionsRepository(apigwManagementApi);
+    const errors = await comms.postToAll(connections, msg);
+    expect(apigwManagementApi.postToConnection.mock.calls.length).toBe(1);
+    expect(errors).toBe(false);
+    expect(apigwManagementApi.postToConnection.mock.calls[0][0]).toEqual({
+      ConnectionId: "1a",
+      Data: JSON.stringify(msg),
+    });
+  });
+
+  test("Posts messages to all connections", async () => {
+    const apigwManagementApi = {
+      postToConnection: fnSuccessReq(),
+    };
+    const connections = [{ id: "1a" }, { id: "1b" }];
+    const comms = new ConnectionsRepository(apigwManagementApi);
+    const errors = await comms.postToAll(connections, "foo bar");
+    expect(apigwManagementApi.postToConnection.mock.calls.length).toBe(2);
+    expect(errors).toBe(false);
+    expect(apigwManagementApi.postToConnection.mock.calls[0][0]).toEqual({
+      ConnectionId: "1a",
+      Data: '"foo bar"',
+    });
+    expect(apigwManagementApi.postToConnection.mock.calls[1][0]).toEqual({
+      ConnectionId: "1b",
+      Data: '"foo bar"',
+    });
+  });
+});
---
+++ lambdas/opt/phase10/repositories/ConnectionsRepository.js
@@ -0,0 +1,35 @@
+class ConnectionsRepository {
+  constructor(apigwManagementApi) {
+    this.apigwManagementApi = apigwManagementApi;
+  }
+
+  async postToAll(connections, message) {
+    const data = JSON.stringify(message);
+    let hasErrors = false;
+    const results = connections.map(async (connection) => {
+      try {
+        if (connection.id !== null) {
+          await this.apigwManagementApi
+            .postToConnection({
+              ConnectionId: connection.id,
+              Data: data,
+            })
+            .promise();
+        }
+      } catch (err) {
+        if (err.statusCode === 410) {
+          connection.id = null;
+          hasErrors = true;
+        } else {
+          throw err;
+        }
+      }
+    });
+
+    await Promise.all(results);
+
+    return hasErrors;
+  }
+}
+
+module.exports = ConnectionsRepository;
