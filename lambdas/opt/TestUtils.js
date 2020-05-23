ad281e612a6b5c08dd1c1e65a97f2351f6ccc86e
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-05-23 18:57:13 +0200

    Moved common code to TestUtils

--- lambdas/opt/TestUtils.js
+++ lambdas/opt/TestUtils.js
@@ -16,6 +16,57 @@ const fnSuccess = (resp) =>
 const fnError = (err) =>
   jest.fn(() => new Promise((_, reject) => setTimeout(() => reject(err), 100)));
 
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      };
+    }
+  },
+  toBeOneOf(received, items) {
+    const pass = items.includes(received);
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be one of (${items.join(", ")})`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be one of (${items.join(", ")})`,
+        pass: false,
+      };
+    }
+  },
+});
+
+beforeEach(() => {
+  console.info("Before each...");
+  console.error = jest.fn();
+  console.warn = jest.fn();
+  console.info = jest.fn();
+  console.log = jest.fn();
+});
+
+afterEach(() => {
+  console.error.mockRestore();
+  console.warn.mockRestore();
+  console.info.mockRestore();
+  console.log.mockRestore();
+  console.info("after each...");
+});
+
 module.exports = {
   fnSuccessReq,
   fnErrorReq,
